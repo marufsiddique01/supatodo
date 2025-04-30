25b54b4d65289e402bb0a57747f9fbb7635bc7c2
Maruf <siddiquemaruf7@gmail.com>
2025-04-30 17:03:05 +0600

    step-5 update and delete

--- app/todos/actions.ts
+++ app/todos/actions.ts
@@ -1,14 +1,15 @@
 "use server";
 
+import { Todo } from "@/types/custom";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 
-export async function addTodo(fromData: FormData) {
+export async function addTodo(formData: FormData) {
   const supabase = createClient();
-  const text = fromData.get("todo") as string | null;
+  const text = formData.get("todo") as string | null;
 
   if (!text) {
-    throw new Error("Todo text is required");
+    throw new Error("Text is required");
   }
 
   const {
@@ -16,15 +17,60 @@ export async function addTodo(fromData: FormData) {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    throw new Error("User is not logged");
+    throw new Error("User is not logged in");
   }
+
   const { error } = await supabase.from("todos").insert({
     task: text,
     user_id: user.id,
   });
 
   if (error) {
-    throw new Error("Error creating todo");
+    throw new Error("Error adding task");
+  }
+
+  revalidatePath("/todos");
+}
+
+export async function deleteTodo(id: number) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase.from("todos").delete().match({
+    user_id: user.id,
+    id: id,
+  });
+
+  if (error) {
+    throw new Error("Error deleting task");
+  }
+
+  revalidatePath("/todos");
+}
+
+export async function updateTodo(todo: Todo) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase.from("todos").update(todo).match({
+    user_id: user.id,
+    id: todo.id,
+  });
+
+  if (error) {
+    throw new Error("Error updating task");
   }
 
   revalidatePath("/todos");
